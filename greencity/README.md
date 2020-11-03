30f82759693cc01f98e6293880e2cda73ff172b0
AlexiaTdev <73832098+AlexiaTdev@users.noreply.github.com>
2020-11-03 10:17:44 +0100

    readme

--- greencity/README.md
+++ greencity/README.md
@@ -1,22 +1,16 @@
-# math-quiz
+# GreenCity
 
 ### introduction
-this is a simple multiplication game with one question with four option and you have only 60 seconds. 
-![2018-08-12](https://user-images.githubusercontent.com/37179627/44004646-27eb7d86-9e83-11e8-9d20-63eda22d1a5a.png)
-
-![2018-08-12 1](https://user-images.githubusercontent.com/37179627/44004664-8644eac0-9e83-11e8-8ba4-e971ce33623d.png)
+simple game where you are the mayor of GreenCity. You must take politicall decisions to rule the city and improve the ecology.
 
 
 ### intruction
 
-first go to the quiz link : https://tvibhu12.github.io/math-quiz/
-then you see the quiz . 
-click on the start button for start the game.
-then a random math multiplication problem will appear with 4 option . which one is right and other three options are wrong. when you click on a right option you will get 1 point. you have only 60 sec.... so good luck and score high. (y) 
+
 
 ### language
 
-i use three language for this quiz
-1. HTML (desgin)
+i use three languages for this quiz
+1. HTML (design)
 2. CSS (styling)
 3. JavaScript (functionality)
--- greencity/main.js
+++ greencity/main.js
@@ -2,25 +2,30 @@ let playing=false;
 let score=0;
 let action;
 let timeremain;
+
+let carte=['maquestion1', [['proposition1', [("stat1","val"), ("stat","val")]], ["proposition2", [("stat1","val"), ("stat","val")]], ["proposition3", [("stat1","val"), ("stat","val")]], ["proposition4", [("stat1","val"), ("stat","val")]]]];
+
+
+
+
 document.getElementById('start').onclick=function(){
     
     if(playing===true)
         {
-           location.reload(); 
+           location.reload();
+           genQA();
         }
     else
     {
         playing=true;
-        document.getElementById('scoreValue').innerHTML = score ;
-        document.getElementById('time').style.display="block"; 
-        timeremain=60;
+        //document.getElementById('scoreValue').innerHTML = score ;
+        //document.getElementById('time').style.display="block"; 
+        //timeremain=60;
         genQA();
-        document.getElementById('start').innerHTML="Reset Game";
+        //document.getElementById('start').innerHTML="Reset Game";
        
-        startCount();
+        //startCount();
     }
-    
-    
     
 }
 
@@ -36,26 +41,30 @@ function startCount()
                document.getElementById('time').style.display="none";
                gameOver();
            }
-   },1000);
+   },10);
 }
 
 
 function gameOver()
 {
-    
     document.getElementById('gameover').style.display="block";
     document.getElementById('scoreno').innerHTML=score;
 }
 
 function genQA() {
 
-    var x=Math.round(10*Math.random());
+    /**var x=Math.round(10*Math.random());
     var y=Math.round(10*Math.random());
     correctAnswer= x*y;
     document.getElementById('qtn').textContent=x +' x '+y;
     var correctPostion= 1+Math.round(3*Math.random());
-    document.getElementById('option'+correctPostion).innerHTML=correctAnswer;
+    document.getElementById('option'+correctPostion).innerHTML=correctAnswer;**/
 
+    document.getElementById('qtn').textContent= 'essai';
+    document.getElementById('note').textContent= carte[0];
+    for (i=0; i<5; i++) {
+        document.getElementById('option'+ (i+1)).textContent= carte[1][i];
+    }
 }
 
 
@@ -65,7 +74,9 @@ for(i=1;i<5;i++)
 
         if(playing===true)
         {
-            if(this.innerHTML==correctAnswer)
+            
+
+            /**if(this.innerHTML==correctAnswer)
             {
                 score++;
                 document.getElementById('scoreValue').innerHTML = score ;
@@ -82,7 +93,7 @@ for(i=1;i<5;i++)
                 setTimeout(function(){
                     hide('try');
                 },1000);
-            }
+            }*/
         }
     }
 }
